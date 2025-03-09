1f5c8555d44179bbbdb593f338d6420c6c21587c
sameerbulakh <sameerbulakh@gmail.com>
2025-03-10 08:24:55 +1100

    Add back Slider and OurCourses components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import WhatWeDo from './components/WhatWeDo';
 import HowWeDoIt from './components/HowWeDoIt';
+import Slider from './components/Slider';
+import OurCourses from './components/OurCourses';
 import OurNetwork from './components/OurNetwork';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
@@ -16,6 +18,8 @@ function App() {
         <Hero />
         <WhatWeDo />
         <HowWeDoIt />
+        <Slider />
+        <OurCourses />
         <OurNetwork />
         <AboutUs />
         <ContactUs />
